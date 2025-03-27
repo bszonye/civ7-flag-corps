32676837c6d3eead933b1bc3c4449c42f8a6a1f6
B. Szonye <bszonye@gmail.com>
2025-03-27 05:03:41 -0700

    configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": [
+                "warn",
+                {
+                    "varsIgnorePattern": "^_",
+                    "argsIgnorePattern": "^_",
+                }
+            ]
+        },
+        languageOptions: {
+            globals: {
+                console: "readonly",
+            }
+        }
+
+    }
+];
---
+++ ui/unit-flags/bz-unit-flags.js
@@ -0,0 +1,1 @@
+xconsole.warn(`TRIX`);
