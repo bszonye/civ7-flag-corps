d25030674c3dfe47caf1eab26422e87619493877
B. Szonye <bszonye@gmail.com>
2025-03-28 02:40:10 -0700

    code cleanup

--- ui/unit-flags/bz-unit-flags.js
+++ ui/unit-flags/bz-unit-flags.js
@@ -25,44 +25,48 @@ if (bzFlagCorpsOptions.noShadow) {
     document.body.classList.remove("bz-flags-no-shadow");
 }
 
+function isVillage(loc) {
+    for (const item of MapConstructibles.getHiddenFilteredConstructibles(loc.x, loc.y)) {
+        const cons = Constructibles.getByComponentID(item);
+        const info = GameInfo.Constructibles.lookup(cons.type);
+        if (info.ConstructibleType == "IMPROVEMENT_VILLAGE") return true;
+    }
+    return false;
+}
+
 const UFMproto = UnitFlagManager.prototype;
 const UFM_onInitialize = UFMproto.onInitialize;
 UFMproto.onInitialize = function(...args) {
     UFM_onInitialize.apply(this, args);
-    console.warn(`TRIX ONINIT ${UnitFlagManager.instance}`);
     engine.on('DiplomacyEventStarted', (_data) => { this.requestFlagsRebuild() });
     engine.on('DiplomacyEventEnded', (_data) => { this.requestFlagsRebuild() });
 };
 
 const _onRecalculateFlagOffsets = UFMproto.onRecalculateFlagOffsets;
 UFMproto.onRecalculateFlagOffsets = function() {
-    console.warn(`TRIX RECALC`);
     for (const plotIndex of this.plotIndicesToCheck) {
         const loc = GameplayMap.getLocationFromIndex(plotIndex);
         const units = MapUnits.getUnits(loc.x, loc.y);
         const position = { x: 0, y: 0 };
+        // dimensions
+        const ycity = 8;
+        const ytown = 24;
+        const icon = 33;  // icon width (healthbar)
+        const offset = icon + 3;  // x-offset between icons
+        const width = icon + (units.length - 1) * offset;
+        const xorigin = -icon/4;  // origin for centering
         // is there a city or town banner?
         const cityID = MapCities.getCity(loc.x, loc.y);
         const city = cityID && Cities.get(cityID);
         if (city) {
-            position.y = city.isTown ? 24 : 8;
-        } else {
-            const village =
-                MapConstructibles.getHiddenFilteredConstructibles(loc.x, loc.y)
-                .map(c => Constructibles.getByComponentID(c))
-                .map(c => GameInfo.Constructibles.lookup(c.type))
-                .some(c => c.ConstructibleType == "IMPROVEMENT_VILLAGE");
-            if (village) position.y = 24;
-            console.warn(`TRIX VILLAGE=${position.y}`);
+            position.y = city.isTown ? ytown : ycity;
+        } else if (isVillage(loc)) {
+            position.y = ytown;
         }
-        // TODO: villages
         for (let u = 0; u < units.length; u++) {
             const unitFlag = UnitFlagManager.instance.getFlag(units[u]);
             if (unitFlag) {
-                console.warn(`TRIX CITY=${JSON.stringify(city)}`);
-                // TODO: enemy flags are always wider
-                const spacing = bzFlagCorpsOptions.noShadow ? 24 : 36;
-                position.x = spacing * (u - 1/2);
+                position.x = xorigin - width/2 + offset * u;
                 unitFlag.updatePosition(position);
             }
             else {
@@ -80,11 +84,9 @@ const IUFproto = IndependentPowersUnitFlag.prototype;
 const GUF_onAttach = GUFproto.onAttach;
 GUFproto.onAttach = function(...args) {  // GeneralUnitFlag only
     GUF_onAttach.apply(this, args);
-    console.warn(`TRIX GUF ONATTACH ${this}`);
     this.realizeAffinity();
 };
 GUFproto.updatePosition = IUFproto.updatePosition = function(position) {
-    // console.warn(`TRIX UPDATE ${position}`);
     if (this.unitContainer && this.flagOffset != position) {
         this.flagOffset = position;
         this.unitContainer.style.left = Layout.pixels(position.x);
@@ -124,7 +126,8 @@ IUFproto.realizeUnitHealth = function(...args) {
     if (this.unitHealthBarInner) {
         const health = this.unit.Health;
         const damage = (health.maxDamage - health.damage) / health.maxDamage;
-        this.unitHealthBarInner.style.widthPERCENT = utils.clamp(damage, 0, 1) * 85;
+        const MAX = 89;  // max-width of unit health bar
+        this.unitHealthBarInner.style.widthPERCENT = utils.clamp(damage, 0, 1) * MAX;
     }
 }
 // undo other patches
@@ -132,16 +135,14 @@ IUFproto.realizeUnitHealth = function(...args) {
 const override = () => {
     const overrides = [
         "/sukritacts_simple_ui_adjustments/ui/unit-flags/suk-unit-flags.js",
-        "foo",
     ];
     for (const mod of overrides) {
         import(mod)
             .then(_mod => console.warn(`bz-unit-flags: override=${mod}`))
-            .catch((_err) => console.warn(`bz-unit-flags: skipped=${mod}`));
+            .catch((_err) => null);
     }
-    console.warn(`TRIX INIT`);
+    // restore standard implementation
     GUFproto.checkUnitPosition = IUFproto.checkUnitPosition = function(unit) {
-        // console.warn(`TRIX OK`);
         UnitFlagManager.instance.recalculateFlagOffsets(unit.location);
     }
 };
