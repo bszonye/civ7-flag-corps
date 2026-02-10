c0b80c5834e80ccd266e89ded38f12b10723fa52
B. Szonye <bszonye@gmail.com>
2026-02-10 13:37:53 -0800

    simplify city-banners decorator

--- ui/city-banners/bz-city-banners.js
+++ ui/city-banners/bz-city-banners.js
@@ -1,6 +1,5 @@
 import bzFlagCorpsOptions from '/bz-flag-corps/ui/options/bz-flag-corps-options.js';
 import bzCityTooltip from '/bz-flag-corps/ui/tooltips/bz-city-tooltip.js';
-import { bzCityBannerManager } from '/bz-flag-corps/ui/city-banners/bz-city-banner-manager.js';
 
 // color palette
 const BZ_COLOR = {
@@ -905,7 +904,7 @@ export class bzCityBanner {
     onAttributeChanged(_name, _prev, _next) { }
 }
 function refreshAllBanners() {
-    const banners = bzCityBannerManager.instance?.banners;
+    const banners = window.banners;
     if (!banners) {
         console.warn(`bz-city-banners: no banners to refresh`);
         return;
